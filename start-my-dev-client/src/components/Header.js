c4df4f0718683cb5390fb83ee7e623f2271b86dd
Sai Annam <107242846+otaku0304@users.noreply.github.com>
2025-07-10 00:25:05 +0530

    feat: Implement dark mode toggle in Header component

--- start-my-dev-client/src/components/Header.js
+++ start-my-dev-client/src/components/Header.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom"; // 👈 import Link
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-import githubIcon from "../assets/github.png";
 import { FaGithub } from "react-icons/fa";
 
 const Header = () => {
